a989ffac4047a4a24fc56ce82302e3f5777f5228
Versus1478 <svider.vladyslav@gmail.com>
2026-03-16 19:46:08 +0100

    Removed old screenshots and added new + routes fix

--- routes/api.php
+++ routes/api.php
@@ -5,23 +5,19 @@ use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('notes/stats/status', [NoteController::class, 'statsByStatus']);
-//Route::get('notes/pinned', [NoteController::class, 'pinned']);
+Route::get('notes/pinned', [NoteController::class, 'pinnedNotes']);
+Route::get('notes/recent/{days?}', [NoteController::class, 'recentNotes']);
 Route::get('notes-actions/search', [NoteController::class, 'search']);
-Route::patch('notes/actions/archive-old-drafts', [NoteController::class, 'archiveOldDrafts']);
-Route::get('users/{user}/notes', [NoteController::class, 'userNotesWithCategories']);
 
 Route::apiResource('notes', NoteController::class);
 Route::apiResource('categories', CategoryController::class);
 
+Route::patch('notes/actions/archive-old-drafts', [NoteController::class, 'archiveOldDrafts']);
 Route::patch('notes/{id}/pin', [NoteController::class, 'pin']);
 Route::patch('notes/{id}/unpin', [NoteController::class, 'unpin']);
-
 Route::patch('notes/{id}/archive', [NoteController::class, 'archive']);
 Route::patch('notes/{id}/publish', [NoteController::class, 'publish']);
 
+Route::get('users/{user}/notes', [NoteController::class, 'userNotesWithCategories']);
 Route::patch('users/{user}/notes/count', [NoteController::class, 'userNoteCount']);
-
-Route::get('notes/pinned', [NoteController::class, 'pinnedNotes']);
-Route::get('notes/recent/{days?}', [NoteController::class, 'recentNotes']);
 Route::get('users/{user}/draft-notes', [NoteController::class, 'userDraftNotes']);
-
